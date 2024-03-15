4ee182b8899dd60c6b02e1462771c8578745af41
goldknight334 <149010644+goldknight334@users.noreply.github.com>
2024-03-15 14:42:33 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,17 @@
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueNumber=Number(score)
+console.log((typeof valueNumber));
+console.log(valueNumber)
+
+// "33"=> 33
+// "33abc" => NaN(Not A Number)
+
+let isLoggedIn=1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true    0=> false
+// "Aman" => true
