8aa6918560cebaa981be018dcc8bf61491b8f52b
goldknight334 <149010644+goldknight334@users.noreply.github.com>
2024-03-20 14:21:48 +0000

    data types introduction

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,24 @@
+//Primitive data types
+// 7 types : Boolean,BigInt, String, Number, null, undefined, Symbol
+
+
+const  Num= 110
+const id = Symbol('1223')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+//Reference(Non Primitive)
+//Array, Objects, Functions
+
+const heros = ["Spiderman", "Lucifer", "Hawkeye"];
+
+let myObj = {
+      name : "Aman",
+      Age : 21,
+}
+
+const myFunction = Function{
+    console.log("Hello");
+}
