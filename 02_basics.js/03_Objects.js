179dfb84801a371e0922551829b3af87cdc53bd3
goldknight334 <149010644+goldknight334@users.noreply.github.com>
2024-03-25 04:37:12 +0000

    objects2

--- 02_basics.js/03_Objects.js
+++ 02_basics.js/03_Objects.js
@@ -1,16 +1,34 @@
 //singleton
 
 //object literals
+
+const mySym = Symbol("key1")
+
 const JsUser = {
     name : "Aman",
     age : 18,
     location : "Jaipur",
+    [mySym] : "key1",
     email: "himang1949@gmail.com",
     isLoggedIn: false,
     lastLoggedIn: ["Monday","tuesday"],
 }
 console.log(JsUser.email);
 console.log(JsUser["email"]);
+console.log(mySym);
 
-const mySym = Symbol("key1")
 
+
+JsUser.email = "kbdha@gmail.com"
+// Object.freeze(JsUser)    //this freezes the object and doesnot allow to change
+JsUser.email = "jwwkhkew"
+console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello js user ");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello js user, ${this.name}` );
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
---
+++ 02_basics.js/04_objects.js
@@ -0,0 +1,18 @@
+// const tinderUser = new Object()   //singleton object
+// or you can defined it by another method that is 
+const tinderUser = {}    //this is non singleton object
+
+tinderUser.id = "123"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+console.log(tinderUser);
+
+const regularUser = {
+    email : "some@gmail.com",
+    fullName : {
+        userFullname : 
+        firstName : "Aman"
+        lastName: "Gupta",
+        
+    }
+}
