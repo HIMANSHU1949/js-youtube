f48b2f71eae95a6ff7a208344f5276178a8d5ab8
goldknight334 <149010644+goldknight334@users.noreply.github.com>
2024-03-25 08:07:02 +0000

    objects

--- 02_basics.js/04_objects.js
+++ 02_basics.js/04_objects.js
@@ -11,8 +11,57 @@ const regularUser = {
     email : "some@gmail.com",
     fullName : {
         userFullname : 
-        firstName : "Aman"
+        {
+            
+        firstName : "Aman",
         lastName: "Gupta",
+    }
         
     }
 }
+
+console.log(regularUser.fullName.userFullname.firstName)
+
+const obj1 = {1: "a",2: "b"}
+const obj2 = {3: "c",4: "d"}
+
+// const obj3 = {obj1 , obj2}
+// const obj3 = Object.assign(obj1,obj2)
+const obj3 = {...obj1,...obj2}
+console.log(obj3);
+
+
+
+const users ={
+    {id : 1,
+    email: "kjwdbwb@gmail.com",
+    },
+    {
+        id : 1,
+        email: "kjwdbwb@gmail.com",
+    
+    },
+    {
+        id : 1,
+        email: "kjwdbwb@gmail.com",
+    
+    },
+    {
+        id : 1,
+        email: "kjwdbwb@gmail.com",
+    
+    },
+    {
+        id : 1,
+        email: "kjwdbwb@gmail.com",
+    
+    },
+    
+
+}
+user[1].email
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
